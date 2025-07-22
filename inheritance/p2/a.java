ed165920bcc76150dd68fd24a3fd2b67b6d32f26
MarianaGranados090 <mgranadosgonzalez@deloitte.com>
2025-07-22 12:11:33 -0600

    Add files via upload

---
+++ inheritance/p2/a.java
@@ -0,0 +1,9 @@
+package p2;
+
+public class a {
+    public int v1 = 4;
+    public int v2 = 5;
+    public int v3 = 9;
+    protected int v4 = 10; //makes v4 available to
+    //child classes but not to the main file.
+}
---
+++ inheritance/p3/b.java
@@ -0,0 +1,17 @@
+package p3;
+import p2.a;
+
+//b is subclass -child
+//a is superclass -parent
+
+public class b extends a{
+    public int v5 = 10;
+    public int v6 = 20;
+
+    public void someMethod()
+    {
+        v4 = 100;
+    } //inheritance
+
+}
+
