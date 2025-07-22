d30eda34433e7df41b9f9f8d7547eebe1e484d8b
MarianaGranados090 <mgranadosgonzalez@deloitte.com>
2025-07-22 12:10:46 -0600

    main java file for inheritance demo.

---
+++ inheritance/main.java
@@ -0,0 +1,12 @@
+package p1;
+
+import p3.b;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        b b1 = new b();
+        System.out.println(b1.v1);
+    }
+}
