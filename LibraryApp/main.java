018e3da1753333dfff402a39cb5c5dbb063e4b6e
MarianaGranados090 <mgranadosgonzalez@deloitte.com>
2025-07-22 11:55:49 -0600

    main file library app section 13

---
+++ LibraryApp/main.java
@@ -0,0 +1,13 @@
+package libApp;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       LibraryAppf app = new LibraryAppf();
+        // app.searchBy("894393");
+      // app.searchByTitle("dog");
+       app.checkOutBook("399399");
+       app.checkInBook("894393");
+    }
+}
